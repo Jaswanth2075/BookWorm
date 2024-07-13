99009b694b76b5e53a788268ffa10cb2e33b3317
Jaswanth_T <117300459+Jaswanth2075@users.noreply.github.com>
2024-07-13 12:19:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+//console.log("hello world");
+//admin
+//Sh24K4cdbj3dl502
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const router = require("./router/book-router");
+
+app.use(express.json());
+app.use("/books", router);
+
+mongoose.connect("mongodb+srv://admin:Sh24K4cdbj3dl502@cluster0.ja9d8kb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=>console.log("Connect to database"))
+.then(()=>{
+    app.listen(5000);
+})
+.catch((err)=>console.log(err));
